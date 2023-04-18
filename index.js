1e49d5883c410a1907d55041af9dae9bf3da5792
Gunwoo <gunwoo.dev@gmail.com>
2023-04-17 21:03:09 -0300

    Update index.js
    - Add eventEmitter for android
    - Add metering option for events
    - Move NativeEventEmitter for ios

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-import { NativeModules, NativeEventEmitter } from 'react-native';
+import { DeviceEventEmitter, NativeModules, NativeEventEmitter, Platform } from 'react-native';
 const { RNAudioRecord } = NativeModules;
-const EventEmitter = new NativeEventEmitter(RNAudioRecord);
 
 const AudioRecord = {};
 
@@ -9,16 +8,25 @@ AudioRecord.start = () => RNAudioRecord.start();
 AudioRecord.stop = () => RNAudioRecord.stop();
 
 const eventsMap = {
-  data: 'data'
+  data: 'data',
+  metering: 'metering',
 };
 
 AudioRecord.on = (event, callback) => {
   const nativeEvent = eventsMap[event];
+
   if (!nativeEvent) {
     throw new Error('Invalid event');
   }
-  EventEmitter.removeAllListeners(nativeEvent);
-  return EventEmitter.addListener(nativeEvent, callback);
+
+  if (Platform.OS === 'android') {
+    const EventEmitter = DeviceEventEmitter.addListener(nativeEvent, callback);
+    return EventEmitter;
+  } else {
+    const EventEmitter = new NativeEventEmitter(RNAudioRecord);
+    EventEmitter.removeAllListeners(nativeEvent);
+    return EventEmitter.addListener(nativeEvent, callback);
+  }
 };
 
 export default AudioRecord;
